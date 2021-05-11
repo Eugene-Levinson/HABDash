8073bdcaf0ed43aa92034e1b2ac7c5f6958f3af4
Eugene Levinson <snow-bug@yandex.ru>
2021-05-11 19:28:12 +0000

    switched to google secrets manager

--- src/app/config/common.js
+++ src/app/config/common.js
@@ -1,5 +1,8 @@
 var config = {};
 
 config.DEFAULT_MODE = "dev"
+config.GCLOUD_PROJECT_NUMBER = "1041773824348"
+config.SECRETS_PATH = `projects/${config.GCLOUD_PROJECT_NUMBER}/secrets`
+config.SECRET_VERSION = "latest"
 
 module.exports = config;
---
+++ src/app/config/secrets.js
@@ -0,0 +1,11 @@
+module.exports.SSL_KEY = process.env.SSL_KEY
+module.exports.SSL_CERT = process.env.SSL_CERT
+module.exports.DB_HOST = process.env.DB_HOST
+module.exports.DB_USER = process.env.DB_USER
+module.exports.DB_PASSWORD = process.env.DB_PASSWORD
+module.exports.DB_NAME = process.env.DB_NAME
+module.exports.DB_USER_TABLE = process.env.DB_USER_TABLE
+module.exports.DB_COOKIE_TABLE = process.env.DB_COOKIE_TABLE
+
+
+
