6ab55c529d983c64b14bd6db5fce767ab76f3e98
Eugene Levinson <snow-bug@yandex.ru>
2021-11-13 01:55:54 +0000

    added edit button and description to dashboard

--- src/app/static/public/scripts/dashboard.js
+++ src/app/static/public/scripts/dashboard.js
@@ -35,18 +35,50 @@ async function load_dashboard_data() {
 
         let main_body = document.getElementById('main-body');
 
-        
-        
+        var flight_info;
+        // for every one of my flights
         for (i in my_flights){
-            main_body.innerHTML += `<a href="flights/${my_flights[i]}" style="background-color:white; margin: 2px;">
-                                        <article class="flight-article" style=" width: 100%; height: 8ch; display: flex; align-items: center; "> 
-                                            <div style="font-size: large; color: black; padding-left: 10px; width: 100%;">
-                                                ${my_flights[i]} (<small>More data coming soon</small>)
-                                            </div>
-                                        </article>
-                                    </a> `
-        }
 
+
+            // Get more info about the flight
+            try {
+                let url = 'https://www.habdash.org/api/get-flight/' + my_flights[i];
+
+                var flight_info = {description: "Couldn't load"};
+
+                let data = await fetch(url);
+
+                // check if responce status is 200
+                if (data.status == 200) {
+                    flight_info = await data.json();
+                    console.log(flight_info);
+                }
+
+            } catch (e){
+                console.log(e)
+
+            } finally {
+
+                // display a card with that flight
+                main_body.innerHTML += `<a href="flights/${my_flights[i]}" style="background-color:white; margin: 2px; ">
+                            <article class="flight-article" style=" width: 100%; min-height: 8ch; display: flex; align-items: center; "> 
+                                <div style="font-size: large; color: black; padding-left: 10px; width: 100%; word-wrap: break-word;">
+                                    <div class="row">
+                                        <div class="col-xl-4 mt-md-0 mt-3">
+                                            ${my_flights[i]} 
+                                        </div>
+                                        <div class="col-xl-8 mt-md-0 mt-3 ">
+                                            <small>Description: ${flight_info.description}</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+                        </a>
+                        <a href="/" class="btn btn-warning" role="button" style="margin-bottom: 3ch; margin-top: 1ch;" >Edit Doc</a>`
+            }
+            
+            
+        }
 
 
     // catch errors
